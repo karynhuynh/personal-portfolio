0482d3bc26914da3132ec91e773abbdae66b30dc
karynhuynh <79120906+karynhuynh@users.noreply.github.com>
2021-06-04 21:35:11 -0700

    Removed old styles and js files

--- src/script.js
+++
@@ -1,7 +0,0 @@
-let arrow = document.querySelector(".arrow-more");
-arrow.addEventListener("mouseover", displayText);
-
-function displayText() {
-  let header = document.querySelector(".more-text");
-  header.innerHTML = "LEARN MORE";
-}
